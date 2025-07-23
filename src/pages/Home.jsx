a259c32ad2b375bb12e3ed6688cbc6188a3a4046
David9191 <jisung9105@gmail.com>
2025-07-23 17:43:09 +0900

    Implement: 랜딩페이지 1차 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { getRandomVerse } from '../data/bibleVerses';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div></div>;
+  const { verse, book, chapter, verseNumber, version } = getRandomVerse();
+
+  return (
+    <>
+      <div className="bible-verse">
+        <h1>샬롬!</h1>
+        <p>To. 사랑하는 하나님의 자녀</p>
+        <div>
+          <p>{verse}</p>
+          <div>
+            <p>
+              {book} {chapter}:{verseNumber} - {version}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="move-to-where">
+        {/* session이 없으면 아래 2개, 있으면 출석체크 */}
+        <Link to={'/signup'}>회원가입</Link>
+        <Link to={'/signin'}>로그인</Link>
+        <Link to={'/signin'}>출석체크</Link>
+      </div>
+    </>
+  );
 };
 
 export default Home;
