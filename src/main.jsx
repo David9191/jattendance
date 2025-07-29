08793acdf1efba636cdf8a88ad6b0987d88a2c09
David9191 <jisung9105@gmail.com>
2025-07-29 21:58:30 +0900

    Feat: 부서 전연 관리 생성

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css';
 import { router } from './router.jsx';
 import { RouterProvider } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
+import { DepartmentContextProvider } from './contexts/DepartmentContext';
 import { UserProfileContextProvider } from './contexts/UserProfileContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProfileContextProvider>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
-    </UserProfileContextProvider>
+    <DepartmentContextProvider>
+      <UserProfileContextProvider>
+        <AuthContextProvider>
+          <RouterProvider router={router} />
+        </AuthContextProvider>
+      </UserProfileContextProvider>
+    </DepartmentContextProvider>
   </StrictMode>,
 );
