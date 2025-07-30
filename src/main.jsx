a69d5592b016f64d21b4bb6aee3101536f0130d9
David9191 <jisung9105@gmail.com>
2025-07-30 16:56:14 +0900

    수정: 폴더 구조 변경에 따른 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './index.css';
 
 import { router } from './router.jsx';
 import { RouterProvider } from 'react-router-dom';
-import { AuthContextProvider } from './contexts/AuthContext';
-import { DepartmentContextProvider } from './contexts/DepartmentContext';
-import { UserProfileContextProvider } from './contexts/UserProfileContext.jsx';
+import { AuthContextProvider } from './common/contexts/AuthContext';
+import { DepartmentContextProvider } from './common/contexts/DepartmentContext';
+import { UserProfileContextProvider } from './common/contexts/UserProfileContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
